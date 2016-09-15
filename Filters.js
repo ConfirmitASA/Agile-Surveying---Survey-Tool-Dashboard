ba55a5eddffcd9cdd6e18e747af0df4a2b45ae5b
Øyvind Thoner <oyvind.thoner@confirmit.com>
2016-09-15 11:30:36 +0200

    Fix problem with deleted hyphens in Filters.js.

--- Filters.js
+++ Filters.js
@@ -1,11 +1,11 @@
 class Filters {
   static function PackageFilter(context) {
-    var agileSurveyIdParamaterName = agileSurveyId;
+    var agileSurveyIdParamaterName = "agileSurveyId";
     if(context.State.Parameters.IsNull(agileSurveyIdParamaterName)) {
-      throw new Exception(AgileSurveyId cannot be null);
+      throw new Exception("AgileSurveyId cannot be null");
     }
     var agileSurveyId = context.State.Parameters.GetString(agileSurveyIdParamaterName);
 
-    context.Component.Expression = 'packageStatus = available and surveyId=' + agileSurveyId + '';
+    context.Component.Expression = 'packageStatus = "available" and surveyId="' + agileSurveyId + '"';
   }
 }
